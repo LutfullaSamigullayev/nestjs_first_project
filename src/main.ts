4b016a9d1e22a8625d694977b476c7675ee86d6d
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-11-23 21:29:23 +0500

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { InternalServerErrorException } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  try {
+    const app = await NestFactory.create(AppModule);
+    await app.listen(3000, () => {
+      console.log("server ishladi...")
+    });
+  } catch(error) {
+    throw new InternalServerErrorException(error.message)
+  }
 }
 bootstrap();
